14edd629cc31797b4a62e4ae2d2682a24844ae57
LMFC <luismiguel.fernandez@murciaeduca.es>
2026-09-17 14:19:54 +0200

    Primer ejercicio en marcha

---
+++ ej01-variables/script.js
@@ -0,0 +1,6 @@
+//1. localizar el botón que va a ser pulsado por el usuario
+const botonCalcularEdad = document.getElementById("calcularBtn")
+botonCalcularEdad.addEventListener("click", function(){
+    //
+
+} )
