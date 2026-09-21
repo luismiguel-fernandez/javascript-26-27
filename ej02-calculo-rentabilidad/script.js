ab57b79e0ff09112d49e04ef18f0fefc50f4d4ff
luismiguel-fernandez <luismiguel-fernandez@murciaeduca.es>
2026-09-21 10:20:30 +0200

    ej02 propuesto

---
+++ ej02-calculo-rentabilidad/script.js
@@ -0,0 +1,20 @@
+/*
+Enunciado:
+
+Elabora un programa en Javascript que pida al usuario:
+    Una cantidad de euros.
+    Un porcentaje de rentabilidad anual.
+    Un número de años de duración del depósito.
+
+A continuación, debe mostrar por pantalla:
+    La evolución del depósito inicial a lo largo de los años establecidos.
+    La rentabilidad total al acabar la duración del depósito.
+
+Aclaraciones sobre datos admitidos:
+    Cantidad en euros: un número entero positivo mayor o igual que 100.
+    Rentabilidad anual: un número decimal positivo entre 0 y 100 admitiendo en la entrada una o más cifras decimales pero a continuación se redondeará a 2 cifras decimales.
+    Duración del depósito: un número entero positivo mayor que cero.
+
+Informar de las causas del error si el usuario introduce un dato no permitido.
+
+*/
