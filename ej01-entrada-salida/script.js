099789966b3171e2ce5e154fde0043a662359394
luismi <luismiguel.fernandez@murciaeduca.es>
2026-09-21 10:04:40 +0200

    calculando edad

---
+++ ej01-entrada-salida/script.js
@@ -0,0 +1,8 @@
+//1. Localizar el botón que va a ser pulsado por el usuario
+const botonCalcularEdad = document.getElementById("calcularBtn")
+
+//2. Asociarle código al evento "el usuario hace clic sobre dicho botón"
+botonCalcularEdad.addEventListener("click", function(){
+    //
+
+} )
